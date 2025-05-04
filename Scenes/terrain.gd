049ca8e023171f94cdca26e8a09ea08a56989c1a
DragonDev22 <dragon22gaming.yt@gmail.com>
2025-05-05 08:04:44 +0930

    did stuff

--- Scenes/terrain.gd
+++ Scenes/terrain.gd
@@ -2,6 +2,18 @@ extends StaticBody2D
 
 @onready var terrain_polygon = $Terrain
 @onready var player = $"../Player"
+@onready var save_file = FileAccess.open("user://level1.save", FileAccess.WRITE_READ)
 
 func _ready() -> void:
-	pass
+	if FileAccess.file_exists("user://level1.save"):
+		print_debug("file exists!")
+	save("Level1")
+
+func save(level: String) -> void:
+	var polygons: Array[Vector2]
+	if level == "Level1":
+		print_debug("Saving " + level)
+		for polygon in terrain_polygon.polygon:
+			polygons.append(polygon)
+		save_file.store_var(polygons)
+		save_file
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,14 +2,15 @@ extends RigidBody2D
 
 @onready var front_wheel = $Front_Wheel/Wheel
 @onready var back_wheel = $Back_Wheel/Wheel
+@export_category("Da Car")
+@export var acceleration := 0.06
+@export var SPEED : float = 22.0
 
 func _physics_process(delta: float) -> void:
-	var SPEED := 32
-	
 	if Input.is_action_pressed("right"):
-		front_wheel.angular_velocity = SPEED
-		back_wheel.angular_velocity = SPEED
+		front_wheel.angular_velocity = lerp(front_wheel.angular_velocity, SPEED, acceleration)
+		back_wheel.angular_velocity = lerp(back_wheel.angular_velocity, SPEED, acceleration)
 	
 	elif Input.is_action_pressed("left"):
-		front_wheel.angular_velocity = -SPEED
-		back_wheel.angular_velocity = -SPEED
+		front_wheel.angular_velocity = lerp(front_wheel.angular_velocity, -SPEED, acceleration)
+		back_wheel.angular_velocity = lerp(back_wheel.angular_velocity, -SPEED, acceleration)
