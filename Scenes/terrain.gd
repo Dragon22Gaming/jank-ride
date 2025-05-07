3d1bfdbd8e8808118cca51ffb8dd3503a097aa20
DragonDev22 <dragon22gaming.yt@gmail.com>
2025-05-07 10:46:42 +0930

    wednesday

--- Scenes/terrain.gd
+++ Scenes/terrain.gd
@@ -1,21 +1,26 @@
 extends StaticBody2D
 
-@onready var terrain_polygon = $Terrain
-@onready var player = $"../Player"
+@onready var terrain= $Terrain
+@onready var collider = $Collider
 @onready var save_file = FileAccess.open("user://level1.save", FileAccess.WRITE_READ)
-
-#formula: ((sin(2*x)*tan(x)-cos(x))/4)+(sin(-x)/tan(0.2*-x))+5+sin(x*0.3)-cos(x*0.6)
 
 func _ready() -> void:
 	if FileAccess.file_exists("user://level1.save"):
 		print_debug("file exists!")
 	save("Level1")
+	#show_terrain()
 
 func save(level: String) -> void:
 	var polygons: Array[Vector2]
 	if level == "Level1":
 		print_debug("Saving " + level)
-		for polygon in terrain_polygon.polygon:
+		for polygon in collider.polygon:
 			polygons.append(polygon)
 		save_file.store_var(polygons)
 		save_file
+
+func show_terrain():
+	var array = PackedVector2Array()
+	for vertex in collider.polygon:
+		array.append(vertex)
+	terrain.polygon = array
